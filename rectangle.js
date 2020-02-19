a296e4becac6dd7ea7f7d6aa44e95a83cc565fdd
lishuangg <1241982864@qq.com>
2020-02-19 20:56:40 +0800

    init

---
+++ rectangle.js
@@ -0,0 +1,21 @@
+$(function(){
+  //get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimenter = $('#perimeter'),
+      $area = $('#erea');
+  
+  /*calc button click event*/
+  $btnCal.click(function(){
+    //get value
+    var w = Number($width.val()),
+        h = Number($height.val());
+
+    //calculate
+    var 
+
+    //output
+    
+  })
+})
